387dcd437b1d41ed97d38308be0ca6c3007d970c
Paul Glorioso <goldcansio@gmail.com>
2021-12-20 18:10:20 -0800

    Added a listener for when the form is submitted, adding the values from the input into an object, added an entry id which increments with each submission.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 /* global data */
 /* exported data */
-// var title = document.querySelector('#title');
+var title = document.querySelector('#title');
 var photo = document.querySelector('#photourl');
-// var notes = document.querySelector('#notes');
-// var subBut = document.querySelector('.subbtn');
+var notes = document.querySelector('#notes');
 var holder = document.querySelector('.photoholder');
+var nextEntryId = 1;
+
 photo.addEventListener('input', function (event) {
   holder.setAttribute('src', photo.value);
+});
+
+document.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var form = document.querySelector('#form');
+  var values = {};
+  values.title = title.value;
+  values.photo = photo.value;
+  values.note = notes.value;
+  values.entryId = nextEntryId;
+  nextEntryId++;
+  form.reset();
 });
