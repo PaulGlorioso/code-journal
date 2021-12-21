0a4cf5e71faac847458bd67b54d98b149d25835d
Paul Glorioso <goldcansio@gmail.com>
2021-12-20 17:35:00 -0800

    Added a listener event to the photo url input to change the photo placeholder to match the value of the input. Added variables for the other inputs commented out.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /* global data */
 /* exported data */
+// var title = document.querySelector('#title');
+var photo = document.querySelector('#photourl');
+// var notes = document.querySelector('#notes');
+// var subBut = document.querySelector('.subbtn');
+var holder = document.querySelector('.photoholder');
+photo.addEventListener('input', function (event) {
+  holder.setAttribute('src', photo.value);
+});
